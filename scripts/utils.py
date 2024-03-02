dd36bd5067a9f293c07e33a0ed4ecb60f2c0151c
Akis  Gazepidis <akis.gazepidis@d-one.ai>
2024-03-02 14:43:42 +0200

    Add final chanmges to etl in data_pipeline

--- scripts/utils.py
+++ scripts/utils.py
@@ -47,6 +47,26 @@ class ParquetToJson:
         except Exception as e:
             print(f"Error while replacing None values with Unknown {e}")
             return np.nan
+        
+    def create_session_id_user_agent_dict(self,session_col, user_agent_col):
+        '''Finction that creates a dictionary with unique session_ids as keys and user_agents as values'''
+        try:
+            session_id_user_agent_tuple_list = [tpl for tpl in set(list(zip(list(user_agent_col), list(session_col)))) if tpl[1]]
+            session_id_user_agent_tuple_dict = {key:value for key, value in session_id_user_agent_tuple_list}
+            return session_id_user_agent_tuple_dict
+        except Exception as e:
+            print(F'Error while creating  the dictionary : {e}')
+            return np.nan
+    
+    def fill_session_id(self,row, dict):
+        '''Function that recieves a df's row for None session_id and a dictionary as inputs and returns then new row with new session_id '''
+        
+        try:
+            row['session_id']  = dict[ row['user_agent']]
+            return row
+        except Exception as e:
+            print(f'Error while finding the new session_id: {e}')
+            return np.nan
 
     def read_dataset(self):
         '''Read the Parquet file data'''
@@ -60,8 +80,8 @@ class ParquetToJson:
         '''Preprocess the Parquet file data'''
         if hasattr(self, 'df'):
             # Do all the preprocessing omn the dataset
-            #self.df[['apartment', 'apartments', 'page', 'requests']] = self.df.apply(lambda row: self.extract_values(row), axis = 1)
-            #self.df.drop('params', axis=1, inplace=True)
+            self.df[['apartment', 'apartments', 'page', 'requests']] = self.df.apply(lambda row: self.extract_values(row), axis = 1)
+            self.df.drop('params', axis=1, inplace=True)
             self.df['ts'] = self.df['ts'].apply(lambda x: str(x) if pd.notnull(x) else '')
             self.df['ts'] = self.df['ts'].apply(lambda x: self.fix_dates1(x) if ('-' in x and x != '') else x)
             self.df['ts'] = self.df['ts'].apply(lambda x: self.fix_dates2(x) if ('/' not in x and x != '') else x)
@@ -72,6 +92,9 @@ class ParquetToJson:
             self.df['page_country'] = self.df['page_country'].apply(self.replace_none_with_unknown)
             self.df['env'] = self.df['env'].apply(self.replace_none_with_unknown)
 
+            self.session_id_user_agent_tuple_dict =  self.create_session_id_user_agent_dict(self.df.session_id, self.df.user_agent)
+
+            self.df = self.df.apply(lambda row: self.fill_session_id(row, self.session_id_user_agent_tuple_dict) if pd.isnull(row['session_id']) else row, axis=1)
             print("Data preprocessed succeddfully")
         else:
             print("Please read the data first.")
