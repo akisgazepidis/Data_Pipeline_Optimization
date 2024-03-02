1a4a2174f56aa982da5c72dbda2fe01bffc3a391
Akis  Gazepidis <akis.gazepidis@d-one.ai>
2024-03-02 13:01:57 +0200

    Add replacing none functions and other fixes

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,17 +1,52 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 class ParquetToJson:
     def __init__(self, filepath):
         self.filepath = filepath
 
     def extract_values(self,row):
+        '''Function that converts a dictionary into a pandas series.'''
         try:
             return pd.Series(row['params'])
         except Exception as e:
-            print(f"Error extracting values: {e}")
+            print(f"Error while extracting values: {e}")
             # Rerurn in case of error
             return pd.Series([np.nan, np.nan, np.nan, np.nan]) 
+    
+    def fix_dates1(self, x):
+        '''Fuction that fix timestamps from '%Y-%m-%dT%H:%M:%S' to 'dd/mm/yyyy HH:MM:SS' format.'''
+        try:
+            dt_object = datetime.strptime(x, '%Y-%m-%dT%H:%M:%S')
+            formatted_timestamp = str(dt_object.strftime('%d/%m/%Y %H:%M:%S'))
+
+            return formatted_timestamp
+        except Exception as e:
+            print(f"Error while converting ts from '%Y-%m-%dT%H:%M:%S' to 'dd/mm/yyyy HH:MM:SS' format: {e}")
+            return np.nan
+        
+    def fix_dates2(self,x):
+        '''Fuction that fix timestamps from floats to 'dd/mm/yyyy HH:MM:SS' format.'''
+        try:
+            dt_object = datetime.utcfromtimestamp(float(x))
+            formatted_timestamp = str(dt_object.strftime('%d/%m/%Y %H:%M:%S'))
+
+            return formatted_timestamp
+        except Exception as e:
+            print(f"Error while converting ts from floats to 'dd/mm/yyyy HH:MM:SS' format: {e}")
+            return np.nan
+    
+    def replace_none_with_unknown(self,x):
+        '''Function that replaces empty values with 'Uknown' value.'''
+        try:
+            if not x:
+                return 'Unknown'
+            else:
+                return x
+        except Exception as e:
+            print(f"Error while replacing None values with Unknown {e}")
+            return np.nan
 
     def read_dataset(self):
         '''Read the Parquet file data'''
@@ -25,9 +60,18 @@ class ParquetToJson:
         '''Preprocess the Parquet file data'''
         if hasattr(self, 'df'):
             # Do all the preprocessing omn the dataset
-            self.df.dropna(inplace=True)
-            self.df[['apartment', 'apartments', 'page', 'requests']] = self.df.apply(lambda row: self.extract_values(row), axis = 1)
-            self.df.drop('params', axis=1, inplace=True)
+            #self.df[['apartment', 'apartments', 'page', 'requests']] = self.df.apply(lambda row: self.extract_values(row), axis = 1)
+            #self.df.drop('params', axis=1, inplace=True)
+            self.df['ts'] = self.df['ts'].apply(lambda x: str(x) if pd.notnull(x) else '')
+            self.df['ts'] = self.df['ts'].apply(lambda x: self.fix_dates1(x) if ('-' in x and x != '') else x)
+            self.df['ts'] = self.df['ts'].apply(lambda x: self.fix_dates2(x) if ('/' not in x and x != '') else x)
+            self.df = self.df[self.df.ts != '']
+            self.df['event_type'] = self.df['event_type'].apply(self.replace_none_with_unknown)
+            self.df['user_country'] = self.df['user_country'].apply(self.replace_none_with_unknown)
+            self.df['user_agent'] = self.df['user_agent'].apply(self.replace_none_with_unknown)
+            self.df['page_country'] = self.df['page_country'].apply(self.replace_none_with_unknown)
+            self.df['env'] = self.df['env'].apply(self.replace_none_with_unknown)
+
             print("Data preprocessed succeddfully")
         else:
             print("Please read the data first.")
